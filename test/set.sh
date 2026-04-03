d969eab7d163543ef6b6cdc084e6997672fce612
jhilikb <69856481+jhilikb@users.noreply.github.com>
2026-04-04 00:00:59 +0530

    Update set.sh

--- test/set.sh
+++ test/set.sh
@@ -1,6 +1,7 @@
 cd ..
 git clone https://github.com/THU-MIG/yolov10.git
 cd yolov10
+wget https://github.com/THU-MIG/yolov10/releases/download/v1.1/yolov10n.pt
 cp /home/test/mytest.py ./
 cp /home/test/libcublas.so.10 /usr/local/cuda/lib64/
 cp /home/test/libcublasLt.so.10 /usr/local/cuda/lib64/
@@ -13,7 +14,11 @@ cp /home/test/libcusparse.so.10 /usr/local/cuda/lib64/
 cp /home/test/libnvToolsExt.so.1 /usr/local/cuda/lib64/
 apt-get update
 apt-get install nano
+pip3 install psutil==5.9.8
 pip3 install matplotlib
 pip3 install PyYAML==6.0.1
 pip3 install tqdm
-pip3 install opencv-python==4.9.0.80
+apt-get install python3-opencv
+pip3 install importlib-metadata
+pip3 install requests
+apt-get install python3-pandas
